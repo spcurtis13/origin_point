30d3ca3f16f286914d9993883cd7baefbe80d8ed
spcurtis13 <38018443+spcurtis13@users.noreply.github.com>
2024-05-19 03:33:45 -0700

    added interface so that req from API would have type

--- src/pages/api/public.ts
+++ src/pages/api/public.ts
@@ -1,15 +1,19 @@
 import { clerkClient } from "@clerk/nextjs";
 import { NextApiRequest, NextApiResponse } from "next";
- 
+
+interface RequestBody {
+  role: string;
+  userId: string;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
- 
-  const { role, userId } = req.body;
- 
+  const { role, userId }: RequestBody = req.body;
+
   await clerkClient.users.updateUserMetadata(userId, {
     publicMetadata: {
       role
     }
-  })
- 
+  });
+
   res.status(200).json({ success: true });
 }
