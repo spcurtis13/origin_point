9b51bb2742c9ebb6d2535b167c4fb47399a6656e
spcurtis13 <38018443+spcurtis13@users.noreply.github.com>
2024-05-19 03:37:35 -0700

    added more stuff to Requestbody in nextapirequest file

--- src/pages/api/public.ts
+++ src/pages/api/public.ts
@@ -7,13 +7,19 @@ interface RequestBody {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { role, userId }: RequestBody = req.body;
+  const body = req.body as RequestBody;
+
+
+
+  const { role, userId } = body;
 
   await clerkClient.users.updateUserMetadata(userId, {
     publicMetadata: {
-      role
-    }
+      role,
+    },
   });
 
   res.status(200).json({ success: true });
 }
+
+
