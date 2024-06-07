1e9737567c36f36f7c70bb9934485c9a503ebe14
spcurtis13 <38018443+spcurtis13@users.noreply.github.com>
2024-06-07 10:37:34 -0700

    added one more field

--- src/pages/app/mentors/index.tsx
+++ src/pages/app/mentors/index.tsx
@@ -74,6 +74,7 @@ export default function Index() {
                   <option value="Aerospace">Aerospace</option>
                   <option value="Real Estate">Real Estate</option>
                   <option value="Fitness/Health">Fitness/Health</option>
+                  <option value="Logistics">Logistics</option>
                 </select>
                 <select
                   className="form-select block w-full px-3 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
